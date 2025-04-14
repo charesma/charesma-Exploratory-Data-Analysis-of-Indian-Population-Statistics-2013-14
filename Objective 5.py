e83dcc5017276700365fb4e81fd50e317ebf7694
charesma <149555451+charesma@users.noreply.github.com>
2025-04-14 21:30:42 +0530

    Update Objective 5.py

--- Objective 5.py
+++ Objective 5.py
@@ -1,5 +1,4 @@
 # Objective 5: Histogram – Distribution of Dried Up Sources 
-
 plt.figure(figsize=(8, 6))
 plt.hist(df['Dried up - No.'], bins=20, color='orange', edgecolor='black')
 plt.title('Histogram: Distribution of Dried Up Sources Across Villages')
