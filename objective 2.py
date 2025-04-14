bd69162416e3ad4a492ec47a8838117414abe79d
charesma <149555451+charesma@users.noreply.github.com>
2025-04-14 13:46:46 +0530

    objective 2.py

---
+++ objective 2.py
@@ -0,0 +1,9 @@
+# Objective 2: Bar Chart – Total Water Source Issues by State
+plt.figure(figsize=(12, 6))
+plt.bar(state_issues.index, state_issues.values, color='skyblue')
+plt.xticks(rotation=90)
+plt.title('Bar Chart: Water Source Issues by State')
+plt.xlabel('State')
+plt.ylabel('Total Issues')
+plt.tight_layout()
+plt.show()
